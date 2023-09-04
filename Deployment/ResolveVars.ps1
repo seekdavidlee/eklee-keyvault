6fa6dfc8cabc01c7797f005f809bb312f7faa74b
David Lee <seekdavidlee@gmail.com>
2023-09-04 08:20:58 -0500

    update to the latest asm

--- Deployment/ResolveVars.ps1
+++ Deployment/ResolveVars.ps1
@@ -3,7 +3,7 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-dotnet tool install --global azsolutionmanager --version 0.1.3-beta
+dotnet tool install --global azsolutionmanager --version 0.1.6-beta
 
 function GetResourceAndSetInOutput {
     param ($SolutionId, $ResourceId, $EnvName, $OutputKey, [switch]$UseId, [switch]$ThrowIfMissing)
